14daca870590806fb85230a3445f82977ef2d63d
Igor Pronin <goldenoronin1999@gmail.com>
2023-02-08 18:58:30 -0800

    add purchase model

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,7 +1,12 @@
 package models
 
+import (
+	"gorm.io/gorm"
+)
+
 // Product is a struct that contains information about the product
 type Product struct {
+	ID          uint
 	Category    string
 	Amount      float64
 	Installment int
@@ -9,6 +14,25 @@ type Product struct {
 
 // Client is a struct that contains information about the client
 type Client struct {
+	ID    uint
 	Name  string
 	Phone string
+}
+
+// Purchase is a struct that contains information about the purchase
+type Purchase struct {
+	ID              uint
+	Product         []Product `gorm:"many2many:purchase_products"`
+	Client          []Client  `gorm:"many2many:purchase_clients"`
+	TotalPayment    float64
+	PaymentPerMonth float64
+}
+
+// InitModels is a function that initializes the database
+func InitModels(db *gorm.DB) error {
+	err := db.AutoMigrate(&Product{}, &Client{}, &Purchase{})
+	if err != nil {
+		return err
+	}
+	return nil
 }
